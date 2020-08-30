431b9c9f845243f587b4a1ecb51456ab3b9d2370
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-08-31 00:13:41 +0600

    ыдшвук ыекудлш

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.banner-section-slider').slick({
+        dots: true,
+        prevArrow:'<button class="banner-section-slider-btn banner-section-slider-btnprev"><img src="/img/left.svg" alt=""></button>',
+        nextArrow:'<button class="banner-section-slider-btn banner-section-slider-btnnext"><img src="/img/right.svg" alt=""></button>'
+    })
+
+})
